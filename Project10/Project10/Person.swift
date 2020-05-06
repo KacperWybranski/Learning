d859f9cab49c2fce194a1d5d009e2f2cb0c045dc
KacperWybranski <63157451+KacperWybranski@users.noreply.github.com>
2020-05-06 13:25:54 +0200

    Uploading done project 10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by test on 05/05/2020.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by test on 04/05/2020.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
