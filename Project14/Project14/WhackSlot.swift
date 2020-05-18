74bb6d5223e7048f2d606305f9e5d4471928aa16
KacperWybranski <63157451+KacperWybranski@users.noreply.github.com>
2020-05-18 14:10:27 +0200

    Add files via upload

---
+++ Project14/Project14/WhackSlot.swift
@@ -0,0 +1,13 @@
+//
+//  WhackSlot.swift
+//  Project14
+//
+//  Created by test on 18/05/2020.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class WhackSlot: SKNode {
+
+}
